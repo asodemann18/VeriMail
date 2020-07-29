29aa35abdeb912c86de60507f13d570d0c6bfa21
Alex Sodemann <asodemann18@gmail.com>
2020-07-29 16:18:11 -0600

    Add functionality to only make fetch request on button click

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,16 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Form from '../Form/Form'
+import { getEmailInfo } from '../../apiCalls'
 
 const App = () => {
-	const [ emails, setEmails ] = useState([]);
+  const [ emails, setEmails ] = useState([]);
+  const [ fileAdded, setFileAdded ] = useState(false);
+
+  useEffect(() => {
+    if(fileAdded) {
+      getEmailData()
+    }
+  });
+  
+  const getEmailData = async () => {
+    try {
+      const data = await getEmailInfo(emails);
+      setEmails(data);
+    } catch(error) {
+      return error; 
+    }
+  }
   
   return (
     <main className="App">
       <header >
         VeriMail
       </header>
-      <Form />
+      <Form setFileAdded={setFileAdded}/>
     </main>
   );
 }
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,40 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import CsvParse from '@vtex/react-csv-parse'
 
-const Form = () => {
+const Form = (props) => {
 	const [ fileInput, setFileInput ] = useState([]);
 
   const handleData = (data) => {
-    // this.setState({fileInput: data})
     setFileInput(data)
   }
 
-  const keys = [
-    "email"
-  ]
+  // const test = () => {
+  //   props.setFileAdded(true)
+  // }
+
+  const key = ["email"]
 
   return(
     <CsvParse
-      keys={keys}
+      keys={key}
       onDataUploaded={handleData}
       render={onChange => {
         return(
           <form>
             <input
-              type="file"
-              name="inputFile"
-              accept=".csv"
-              // value={this.state.emailInput}
+              type='file'
+              name='inputFile'
+              accept='.csv'
               onChange={onChange}
-              aria-label="validate-emails"
+              aria-label='validate-emails'
             />
-              <button 
-                // onClick={() => this.props.getEmailData(this.state.emailInput)}
-                onClick={fileInput}
-                type="button"
-              >
-                Validate
-              </button>
+            <button 
+              onClick={() => props.setFileAdded(true)}
+              // onClick={fileInput}
+              type='button'
+            >
+              Validate
+            </button>
           </form>)
       }}
   />
