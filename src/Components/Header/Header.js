f381de3af1c741bc2eb3de5b0d985bb174c657bb
Alex Sodemann <asodemann18@gmail.com>
2020-07-30 18:35:57 -0600

    Add routing to Header component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
+
+const Header = ({fileAdded}) => {
+
+  return (
+    <header>
+      {!fileAdded && 
+        <Link to='/'>
+          <h1>VeriMail</h1>
+        </Link>
+      }
+      {fileAdded && 
+        <div>
+          <Link to='/verified-emails'>
+            <h1>VeriMail</h1>
+          </Link>
+          <section>
+            <Link to='email-stats'>
+              <h3>Stats</h3>
+            </Link>
+            <Link to='email-details'>
+              <h3>Details</h3>
+            </Link>
+            <Link to='verified-emails'>
+              <h3>Verified List</h3>
+            </Link>
+          </section>
+
+        </div> 
+      }
+    </header>
+  )
+}
+
+export default Header;
