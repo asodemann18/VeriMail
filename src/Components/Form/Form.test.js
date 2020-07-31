d00bef5861bf102db4bd603417906070840f901a
Alex Sodemann <asodemann18@gmail.com>
2020-07-31 10:02:37 -0600

    Add tests to Form Component testing file

--- src/Components/Form/Form.test.js
+++ src/Components/Form/Form.test.js
@@ -8,11 +8,59 @@ describe('Form', () => {
   it('should display a form', () => {
     const mockSetFileAdded = jest.fn()
     const mockSetEmails = jest.fn()
-    const { getByPlaceholderText } = render(
-      <Form 
-       setFileAdded={mockSetFileAdded}
-       setEmails={mockSetEmails}
-      />
+    const { getByPlaceholderText, getByRole } = render(
+      <MemoryRouter>
+        <Form 
+         setFileAdded={mockSetFileAdded}
+         setEmails={mockSetEmails}
+        />
+      </MemoryRouter>
     )
+    
+    const input = getByPlaceholderText('upload csv');
+    const button = getByRole('button', {name: 'Verify'})
+
+    expect(input).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  })
+
+  it('should be able to input a csv in the form', () => {
+    const mockSetFileAdded = jest.fn()
+    const mockSetEmails = jest.fn()
+    const { getByPlaceholderText, getByRole } = render(
+      <MemoryRouter>
+        <Form 
+         setFileAdded={mockSetFileAdded}
+         setEmails={mockSetEmails}
+        />
+      </MemoryRouter>
+    )
+
+    const input = getByPlaceholderText('upload csv');
+    fireEvent.change(input, {target: {files: ['test.csv']}});
+
+    expect(input.files).toEqual(['test.csv'])
+  })
+
+  it('should be able to upload a csv in the form', () => {
+    const mockSetFileAdded = jest.fn()
+    const mockSetEmails = jest.fn()
+    const { getByPlaceholderText, getByRole } = render(
+      <MemoryRouter>
+        <Form 
+         setFileAdded={mockSetFileAdded}
+         setEmails={mockSetEmails}
+        />
+      </MemoryRouter>
+    )
+
+    const input = getByPlaceholderText('upload csv');
+    const button = getByRole('button', {name: 'Verify'})
+
+    fireEvent.change(input, {target: {files: ['test.csv']}});
+    fireEvent.click(button)
+
+    expect(mockSetFileAdded).toHaveBeenCalledWith(true);
+    expect(mockSetEmails).toHaveBeenCalledWith([]);
   })
 })
