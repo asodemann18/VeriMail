877dcf8a8b2ae886a9b3e01aff34a32a4e1efea1
Alex Sodemann <asodemann18@gmail.com>
2020-08-01 10:38:39 -0600

    Change display of verified emails from list to table

--- src/Components/VerifiedEmails/VerifiedEmails.js
+++ src/Components/VerifiedEmails/VerifiedEmails.js
@@ -2,16 +2,30 @@ import React from 'react';
 import './VerifiedEmails.css';
 
 const VerifiedEmails = ({ filteredEmails }) => {
-    const verifiedResults = filteredEmails.map(email => {
+  const verifiedResults = filteredEmails.map(email => {
     return (
-      <li>{email.email}</li>
+      <tr>
+        <td>{email.email}</td>
+      </tr>
     )
   })
 
   return (
-    <section>
-      <ul>{verifiedResults.length && verifiedResults}</ul>
-    </section>
+    // <section>
+    //   <ul>{verifiedResults.length && verifiedResults}</ul>
+    // </section>
+    <table>
+      {/* <thead>
+        <tr>
+          <th>V</th>
+        </tr>
+      </thead> */}
+      <tbody>
+        {/* <tr> */}
+          {verifiedResults.length && verifiedResults}
+        {/* </tr> */}
+      </tbody>
+    </table>
   )
 }
 
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,8 +1,8 @@
 
 export const getEmailInfo = async (email) => {
-  const response = await fetch(`https://apilayer.net/api/check?access_key=c9e97b26d4099cd4e226ce145b24ff95&email=${email}&format=1`)
+  // const response = await fetch(`https://apilayer.net/api/check?access_key=c9e97b26d4099cd4e226ce145b24ff95&email=${email}&format=1`)
   
-  // const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${email}`)
+  const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${email}`)
 
   if(response.ok) {
 		return await response.json();
