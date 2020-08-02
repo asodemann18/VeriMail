6f4ba16b71c89c997125ccfc0276e464133e9bae
Alex Sodemann <asodemann18@gmail.com>
2020-08-02 14:02:35 -0600

    Add styling to table - table width still needs to be fixed

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -5,46 +5,47 @@ const Details = ({emails}) => {
   const emailDetailsTitles = emails.map(title => {
     return (
       <tr>
-        <td>{title.email}</td>
-        <td>{title.did_you_mean}</td>
-        <td>{title.user}</td>
-        <td>{title.domain}</td>
-        <td>{title.format_valid.toString()}</td>
-        <td>{title.mx_found.toString()}</td>
-        <td>{title.smtp_check.toString()}</td>
-        <td>{title.role.toString()}</td>
-        <td>{title.disposable.toString()}</td>
-        <td>{title.free.toString()}</td>
-        <td>{title.score*100}</td>
+        <td className='table-data'>{title.email}</td>
+        <td className='table-data'>{title.did_you_mean}</td>
+        <td className='table-data'>{title.user}</td>
+        <td className='table-data'>{title.domain}</td>
+        <td className='table-data'>{title.format_valid.toString()}</td>
+        <td className='table-data'>{title.mx_found.toString()}</td>
+        <td className='table-data'>{title.smtp_check.toString()}</td>
+        <td className='table-data'>{title.role.toString()}</td>
+        <td className='table-data'>{title.disposable.toString()}</td>
+        <td className='table-data'>{title.free.toString()}</td>
+        <td className='table-data'>{title.score*100}</td>
       </tr>
     )
   })
 
   return (
-    <section className='detail-section'>
-      <h3>Email Details</h3>
+    <section className='detail-container'>
+      <section className='detail-section'>
+      <h3 className='detail-title'>Email Details</h3>
       <table>
         <thead>
           <tr>
-            <th>Email</th>
-            <th>Did you mean?</th>
-            <th>User</th>
-            <th>Domain</th>
-            <th>Valid Format</th>
-            <th>Valid Domain</th>
-            <th>Valid User</th>
-            <th>Role</th>
-            <th>Disposable</th>
-            <th>Free</th>
-            <th>Score</th>
+            <th className='table-header'>Email</th>
+            <th className='table-header'>Did you mean?</th>
+            <th className='table-header'>User</th>
+            <th className='table-header'>Domain</th>
+            <th className='table-header'>Valid Format</th>
+            <th className='table-header'>Valid Domain</th>
+            <th className='table-header'>Valid User</th>
+            <th className='table-header'>Role</th>
+            <th className='table-header'>Disposable</th>
+            <th className='table-header'>Free</th>
+            <th className='table-header'>Score</th>
           </tr>
         </thead>
         <tbody>
           {emailDetailsTitles}
         </tbody>
       </table>
+      </section>
     </section>
-    
   )
 }
 
