f37a1296bad68118357e29ddbbf37a10a63e3b44
Alex Sodemann <asodemann18@gmail.com>
2020-08-02 21:32:54 -0600

    Add PropTypes to VerifiedEmailsComponent

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
 
-const Header = ({fileAdded}) => {
 
+const Header = () => {
   return (
     <header>       
       <div className='uploaded-header'>
